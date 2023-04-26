e9f1a56a5f98608a7950867cde02cee3154a3522
Gradykisibu <kisibugrady3980@gmail.com>
2023-04-26 15:48:01 +0200

    pushing fixed protected routes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,23 +1,21 @@
-import React, { useState, useEffect , useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Box } from "@mui/material";
 import styles from "../../styles/main.module.css";
 import { db } from "@/Firebase/firebase";
 import { query, collection, onSnapshot, doc } from "firebase/firestore";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
-import ContentPasteIcon from "@mui/icons-material/ContentPaste";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { AuthContext } from "../context/AuthContext";
-
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Link from "next/link";
+import InfoIcon from '@mui/icons-material/Info';
 
 
 const Main = () => {
   const [vacationData, setVacationData] = useState([]);
   const [favourite, setFavourite] = useState(false);
-  const { user } = useContext(AuthContext);
-  
+
   useEffect(() => {
     const q = query(collection(db, "Vacations"));
     const unsub = onSnapshot(q, (querySnapshot) => {
@@ -31,15 +29,14 @@ const Main = () => {
     return () => unsub();
   }, []);
 
-  
   useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
 
   const handleSave = () => {
-    console.log("Favourite")
-    setFavourite(true)
-  }
+    console.log("Favourite");
+    setFavourite(true);
+  };
 
   return (
     <Box>
@@ -49,67 +46,64 @@ const Main = () => {
 
       <Box className={styles.cardContainer}>
         {vacationData.map((vacation) => {
-         return(
-          <Box
-          className={styles.singleDestination}
-          key={vacation.id}
-          data-aos="fade-up"
-          data-aos-easing="linear"
-          data-aos-duration="1500"
-        >
-          <Box className={styles.imageDiv}>
-            <img
-              className={styles.image}
-              src={vacation.imgSrc}
-              style={{ width: "300px", height: "200px" }}
-            />
-          </Box>
-
-          <Box className={styles.cardInfo}>
-            <Box>
-              <h4 className={styles.destTitle}>{vacation.destTitle}</h4>
-              <span className={styles.continent}>
-                <LocationOnOutlinedIcon />
-                <span className={styles.name}>{vacation.location}</span>
-              </span>
-            </Box>
-
-            <Box 
-            className={styles.favouriteIcon}
-            onClick={() => handleSave()}
+          return (
+            <Box
+              className={styles.singleDestination}
+              key={vacation.id}
+              data-aos="fade-up"
+              data-aos-easing="linear"
+              data-aos-duration="1500"
             >
-              {
-                favourite ? 
+              <Box className={styles.imageDiv}>
+                <img
+                  className={styles.image}
+                  src={vacation.imgSrc}
+                  style={{ width: "300px", height: "200px" }}
+                />
+              </Box>
 
-                (<FavoriteIcon/>)
-                 : 
-                (<FavoriteBorderIcon/>)
-              }
+              <Box className={styles.cardInfo}>
+                <Box>
+                  <h4 className={styles.destTitle}>{vacation.destTitle}</h4>
+                  <span className={styles.continent}>
+                    <LocationOnOutlinedIcon />
+                    <span className={styles.name}>{vacation.location}</span>
+                  </span>
+                </Box>
+
+                <Box className={styles.moreinfor}>
+                  <Box
+                  className={styles.favouriteIcon}
+                  onClick={() => handleSave()}
+                >
+                  {favourite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                </Box>
+
+                <Box>
+                </Box>
+                <Link href={"/Details/" + vacation.id}>
+                  <InfoIcon/>
+              </Link>
+                </Box>               
+              </Box>
+
+              <Box className={styles.fees}>
+                <Box className={styles.grade}>
+                  <span>
+                    {vacation.grade}
+                    <small>+1</small>
+                  </span>
+                </Box>
+                <Box className={styles.price}>
+                  <h5>R{vacation.price}</h5>
+                </Box>
+              </Box>
+
+              <Box className={styles.description}>
+                <p>{vacation.description}</p>
+              </Box>
             </Box>
-          </Box>
-
-          <Box className={styles.fees}>
-            <Box className={styles.grade}>
-              <span>
-                {vacation.grade}
-                <small>+1</small>
-              </span>
-            </Box>
-            <Box className={styles.price}>
-              <h5>R{vacation.price}</h5>
-            </Box>
-          </Box>
-
-          <Box className={styles.description}>
-            <p>{vacation.description}</p>
-          </Box>
-
-          <Box className={styles.moreinfor}>
-            <p>Details</p>
-            <ContentPasteIcon fontSize="10px" />
-          </Box>
-        </Box>
-         )
+          );
         })}
       </Box>
     </Box>
